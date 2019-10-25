e74edb824be44d7683093611badd5187451f4e37
Archita Singh <55463626+archita924@users.noreply.github.com>
2019-10-25 06:58:14 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,48 @@
+class hangman:
+  def display:
+    print("word of five letter")
+  def num(self,start1,s2,s3,s4,s5):
+    self.start1=""
+    self.s2=""
+    self.s3=""
+    self.s4=""
+    self.s5=""
+   word=hangman()
+   word.start1="c"
+   word.s2="u"
+   word.s3="t"
+   word.s4="i"
+   word.s5="e"
+ class zero():
+  def__init__(self):
+    if word==num:
+      print("YOU WON")
+    else:
+      print("YOU LOST")
+   class q():
+def__init(self):
+  if num.start1==word.start1:
+    print("correct goahead")
+  elif num.start1!=word.start1:
+    print("you are wrong")
+    print("10 more events")
+  elif num.s2==word.s2:
+    print("correct ")
+  elif num.s2!=word.s2:
+    print("9 more chance")
+  elif num.s3==word.s3:
+    print("correct ")
+  elif num.s3!=word.s3:
+    print("8 more chance")
+  elif elif num.s4==word.s4:
+    print("correct ")
+  elif num.s4!=word.s4:
+    print("7 more chance")
+  elif num.s5==word.s5:
+    print("correct ")
+  elif num.s5!=word.s5:
+    print("6 more chance")
+  
+   
+    
+    
